2324b39f1bb86a620094d65f2700ce1a8be8fbf9
강찬혁 <72860075+KChanHyeok@users.noreply.github.com>
2025-05-16 16:04:44 +0900

    first commit

---
+++ myhomego_back/src/main/java/com/example/myhomego_back/MyhomegoBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.myhomego_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyhomegoBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyhomegoBackApplication.class, args);
+	}
+
+}
---
+++ myhomego_back/src/test/java/com/example/myhomego_back/MyhomegoBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myhomego_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyhomegoBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
