73b6c8ffe45578b533653f01f32c7855abf55eec
manbron236 <minjuncho@naver.com>
2025-05-19 10:32:34 +0900

    Fix LoginPage

--- myhomego_front/src/App.jsx
+++ myhomego_front/src/App.jsx
@@ -1,11 +1,20 @@
-import "./App.css";
+// App.jsx
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import MemberMainPage from "./pages/MemberMainPage";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
 import ChatGpt from "./pages/ChatGpt";
 
 function App() {
   return (
-    <div>
-      <ChatGpt />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MemberMainPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/chat" element={<ChatGpt />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ myhomego_front/src/pages/MainPage.jsx
@@ -0,0 +1,14 @@
+// src/pages/LoginPage.jsx
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+// import "./MainPage.css";
+
+export default function MainPage() {
+  
+  return (
+    <div className="main-container">
+      <h2>로그인 성공</h2>
+      
+    </div>
+  );
+}
